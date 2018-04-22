5a6302a9ba147bd0044e69d67ca1c373bc84eb41
Mahfouz Sheikh <mahfouz.alsheikh@gmail.com>
2018-04-21 20:34:31 -0400

    Added the token verification API.

--- api/accounts/urls.py
+++ api/accounts/urls.py
@@ -4,13 +4,13 @@ from django.views.generic.base import TemplateView
 from django.contrib.sitemaps.views import sitemap
 from django.views.generic import RedirectView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from rest_framework_jwt.views import obtain_jwt_token
-from rest_framework_jwt.views import refresh_jwt_token
-from accounts.views import *
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+from accounts.views import Signup, Activate, PasswordResetView, SetNewPassword
 
 urlpatterns = [
     url(r'^login/', obtain_jwt_token),
     url(r'^token-refresh/', refresh_jwt_token),
+    url(r'^token-verify/', verify_jwt_token),
     url(r'^signup/', Signup.as_view(), name="signup"),
     url(r'^activate/', Activate.as_view(), name="activate"),
     url(r'^password_reset/', PasswordResetView.as_view(), name="password_reset"),
