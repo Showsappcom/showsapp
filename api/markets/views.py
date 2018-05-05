2843d663220f5b59b9cbb0d3c90e7d146251e480
Mahfouz Sheikh <mahfouz.alsheikh@gmail.com>
2018-05-05 12:31:59 -0400

    Added a server side item page.

--- api/markets/views.py
+++ api/markets/views.py
@@ -209,8 +209,12 @@ class MyItem(generics.ListAPIView):
             return Response({}, status=404)
 
 
-def item(request,id):
-    item = Item.objects.get(pk=id)
-    context = { "item": item, "stripe_pk": settings.STRIPE['PUBLISHABLE_KEY'], "value": item.good_faith_money * 100 }
+def item(request,seller_id, slug):
+    items = Item.objects.filter(slug=slug, sa_user__id=seller_id)
+    if items.exists():
+        context = { "item": items[0] }
+    else:
+        context = { "item": None }
+
     template = get_template('./markets/item.html')
     return HttpResponse(template.render(context,request))
